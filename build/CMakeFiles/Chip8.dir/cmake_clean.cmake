f3ff5b0d27623785eb143d14b70cb2542719f535
Rami <myersrami17@gmail.com>
2025-02-28 10:02:14 -0600

    Added TODO in main.c

---
+++ build/CMakeFiles/Chip8.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chip8.dir/main.c.o"
+  "CMakeFiles/Chip8.dir/main.c.o.d"
+  "Chip8"
+  "Chip8.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Chip8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
