b972d1a19b810ca6c0fc5e819feb408b5b989b63
Rami <myersrami17@gmail.com>
2025-02-28 10:05:42 -0600

    Committed correct directory

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// TODO: Research binary file reading to read instructions
+// TODO: Implement simple OpenGL window
+// TODO: Start working on instruction decoding
+
+unsigned char memory[4096];
+unsigned char V[16];
+
+int main(void) {
+  return 0;
+}
