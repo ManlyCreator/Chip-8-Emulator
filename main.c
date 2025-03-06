d78d7038fc6e483253cbdce2846cb11b2cfe5efa
Rami <myersrami17@gmail.com>
2025-03-06 16:12:10 -0600

    Implemented basic file opening, updated TODO

--- main.c
+++ main.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// TODO: Research binary file reading to read instructions (CPAMA: p. 539)
 // TODO: Implement simple OpenGL window
 // TODO: Start working on instruction decoding
+#define Byte char
 
 unsigned char memory[4096];
 unsigned char V[16];
 
-int main(void) {
+int main(int argc, char **argv) {
+  Byte *instruction = malloc(sizeof(Byte));
+  FILE *bFile = fopen("../roms/eaty.ch8", "rb");
+
+  if (!bFile) {
+    printf("Could not open file\n");
+    return -1;
+  }
+
+  fread(instruction, sizeof(Byte), 1, bFile);
+  printf("Instruction: %x\n", *instruction);
+
   return 0;
 }
