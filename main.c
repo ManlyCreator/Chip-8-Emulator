2a514c67e4b0d7ed60627e22e16b0ba1f5be5af6
Rami <myersrami17@gmail.com>
2025-04-04 11:18:34 -0500

    Figured out OpenAL bugs, updated TODO to add tone and debug seg faults

--- main.c
+++ main.c
@@ -1,18 +1,26 @@
 // Standard Libraries
+#include <AL/alc.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 // External Libraries
 #include "chip8.h"
+#include <AL/al.h>
 
-// TODO: Sound
+#define SAMPLE_RATE 44100
+#define FREQUENCY 440
+#define DURATION 1.0
+#define NUM_SAMPLES (int)(SAMPLE_RATE * DURATION)
+
+// TODO: Implement OpenAL tone from Audio code snippet
+// TODO: Debug seg faults
+// TODO: Load ROMs from CLI
 
 int main(int argc, char **argv) {
-  Chip8 chip8;
 
-  // Chip8
-  chip8 = chipInitialize();
-  if (!chipLoadROM(&chip8, "../roms/spaceInvaders.ch8"))
+  Chip8 chip8 = chipInitialize();
+  if (!chipLoadROM(&chip8, "../roms/astroDodge.ch8"))
     return -1;
   chipStartMainLoop(&chip8, 16);
 
